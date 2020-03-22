ad7dec3a5938300da07d5d611a7002c3507ae932
kaizoku-o <liahoshaikh@gmail.com>
2020-03-22 17:34:34 +0000

    Massaged the questions and information a bit
    Tested on devel build

--- src/api/quizQuestions.js
+++ src/api/quizQuestions.js
@@ -1,49 +1,50 @@
 var quizQuestions = [
   {
-      question: "Does the novel coronavirus only seriously affects older people?",
-      info:`While statistically older people are more severely affected that is skewed by the fact that 
-	    they are part of the population that have pre-existing medical conditions. 
+      question: "The novel coronavirus only seriously affects older people.",
+      info:`While statistically speaking older people are more severely affected, that is skewed 
+            by the fact that they are a part of the population that tend to have pre-existing 
+	    medical conditions. 
 	    The fact here is that the COVID-19 affects anyone with pre-existing medical conditions.
-	    People of all ages can be infected by the new coronavirus (2019-nCoV). Older people, and
-	    people with pre-existing medical conditions (such as asthma, diabetes, heart disease)
+	    People of all ages can be infected by the new coronavirus. 
+	    Older people, and people with pre-existing medical conditions (such as asthma, diabetes, heart disease)
 	    appear to be more vulnerable to becoming severely ill with the virus.
  	    WHO advises people of all ages to take steps to protect themselves from the virus,
 	    for example by following good hand hygiene and good respiratory hygiene.`,
       answers: [
           {
               type: "True",
-              content: "Yes"
+              content: "True"
           },
 	  {
 	      type: "False",
-	      content: "No"
+	      content: "False"
 	  }
       ],
       correctAnswer: "False"
   },
   {
-      question: "Are antibiotics effective in preventing and treating the new coronavirus?",
-      info: `No, antibiotics do not work against viruses, only bacteria.
-	    The new coronavirus (2019-nCoV) is a virus and, therefore, antibiotics should 
+      question: "Antibiotics are effective in preventing and treating the new coronavirus.",
+      info: `No, antibiotics do not work against viruses. They only work against bacteria.
+	    The new coronavirus is well, a virus and therefore antibiotics should 
             not be used as a means of prevention or treatment.
-	    However, if you are hospitalized for the 2019-nCoV, you may receive antibiotics 
-	    because bacterial co-infection is possible.`,
+	    However, if you are hospitalized for COVID-19, you may receive antibiotics to 
+	    keep you from getting a bacterial co-infection due to your low immunity.`,
       answers: [
           {
               type: "True",
-              content: "Yes"
+              content: "True"
           },
           {
               type: "False",
-              content: "No"
+              content: "False"
           }
       ],
       correctAnswer: "False"
   },
   {
-      question: "There are medicines to treat the new coronavirus?",
-      info:  `To date, there is no specific medicine recommended to prevent or treat the new 
-      	     coronavirus (2019-nCoV). However, those infected with the virus should receive 
+      question: "There are medicines to treat the new coronavirus.",
+      info:  `There is no specific drug to prevent or treat the new 
+      	     coronavirus, yet. However, those infected with the virus should receive 
 	     appropriate care to relieve and treat symptoms, and those with severe illness 
 	     should receive optimized supportive care. Some specific treatments are under 
 	     investigation, and will be tested through clinical trials. WHO is helping to 
@@ -51,34 +52,34 @@ var quizQuestions = [
       answers: [
           {
               type: "True",
-              content: "Yes"
+              content: "True"
           },
           {
               type: "False",
-              content: "No"
+              content: "False"
           }
       ],
       correctAnswer: "False"
   },
   {
-      question: "Wearing a surgical mask will protect you from getting coronavirus?",
+      question: "Wearing a surgical mask will protect you from getting coronavirus.",
       info: `Masks do not prevent new infections. If you are already sick, your doctor may 
       	    ask you to wear one. Otherwise leave masks for health professionals.`,
       answers: [
           {
               type: "True",
-              content: "Yes"
+              content: "True"
           },
           {
               type: "False",
-              content: "No"
+              content: "False"
           }
       ],
       correctAnswer: "False"
   },
   {
-      question: "Coronavirus is the deadliest virus known to man",
-      info: `Although SARS-CoV-2 does appear to be more serious than influenza, it is not 
+      question: "Is coronavirus the deadliest virus known to man?",
+      info: `Although SARS-CoV-2 is 10-35 times more lethal than influenza, it is not 
       	    the deadliest virus that people have faced. Others, such as Ebola, have higher 
 	    mortality rates.`,
       answers: [
@@ -94,7 +95,7 @@ var quizQuestions = [
       correctAnswer: "False"
   },
   {
-      question: "Can eating garlic help prevent infection with the new coronavirus?",
+      question: "Can eating garlic help prevent being infected by the new coronavirus?",
       info: `Garlic is a healthy food that may have some antimicrobial properties. However, 
       	    there is no evidence from the current outbreak that eating garlic has protected
 	    people from the new coronavirus.`,
@@ -111,53 +112,53 @@ var quizQuestions = [
       correctAnswer: "False"
   },
   {
-      question: "Coronavirus cannot survive in Hot and Humid areas?",
+      question: "Coronavirus cannot survive in Hot and Humid areas.",
       info: `There is no proper evidence to suggest that Coronavirus can not survive hot and 
-      humid weather. It has been a widely circulated rumor about Covid-19 that it cannot 
+      humid weather. It has been a widely circulated rumor about the novel coronavirus, that it cannot 
       withstand summers or high-temperature weathers. As per the WHO international Website – 
-      ‘ From the evidence so far, the COVID-19 virus can be transmitted in ALL AREAS, including 
+      ‘From the evidence so far, the COVID-19 virus can be transmitted in ALL AREAS, including 
       areas with hot and humid weather’.`,
       answers: [
           {
               type: "True",
-              content: "Yes"
+              content: "True"
           },
           {
               type: "False",
-              content: "No"
+              content: "False"
           }
       ],
       correctAnswer: "False"
   },
   {
-      question: "Will there be a COVID-19 vaccine available within the next 4 months?",
-      info: `No. Vaccines are under development and trial right now. However it could take 
+      question: "There will be a vaccine available for COVID-19 in the next 1-2 years.",
+      info: `Vaccines are under development and will enter human trials soon. However it could take 
       anywhere between 12 to 18 months for a vaccine to be deployable.`,
       answers: [
           {
               type: "True",
-              content: "Yes"
+              content: "True"
           },
           {
               type: "False",
-              content: "No"
+              content: "False"
           }
       ],
-      correctAnswer: "False"
+      correctAnswer: "True"
   },
   {
-      question: "Wearing rubber gloves while out in public is effective in preventing the new coronavirus infection?",
-      info: `No. Regularly washing your bare hands offers more protection against catching COVID-19 
-      then wearing rubber gloves. You can still pick up COVID-19 contamination on rubber gloves. If 
+      question: "Wearing rubber gloves while out in public is effective in preventing the new coronavirus infection",
+      info: `That statement is incorrecnt. Regularly washing your bare hands offers more protection against catching COVID-19 
+      than wearing rubber gloves. You can still pick up COVID-19 contamination on rubber gloves. If 
       you then touch your face, the contamination goes from your glove to your face and can infect you. `,
       answers: [
           {
               type: "True",
-              content: "Yes"
+              content: "True"
           },
           {
               type: "False",
-              content: "No"
+              content: "False"
           }
       ],
       correctAnswer: "False"
@@ -166,7 +167,8 @@ var quizQuestions = [
       question: "Is it safe to receive a letter or a package from China?",
       info: `Yes, it is safe. People receiving packages from China are not at risk of 
       contracting the new coronavirus. From previous analysis, we know coronaviruses do not 
-      survive long on objects, such as letters or packages`,
+      survive long on objects, such as letters or packages. The maximum they can survive is 48 hours
+      on hard surfaces`,
       answers: [
           {
               type: "True",
